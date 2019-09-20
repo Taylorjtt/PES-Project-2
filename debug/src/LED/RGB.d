d542fefdf200ea944e588d2e343a8efb1cecd11f
John Taylor <john.t.taylor@puregas.com>
2019-09-20 16:01:20 -0600

    fixed untracked files

--- debug/src/LED/RGB.d
+++
@@ -1,3 +0,0 @@
-src/LED/RGB.o src/LED/RGB.d: ../src/LED/RGB.c ../src/LED/RGB.h
-
-../src/LED/RGB.h:
--- debug/src/main.d
+++
@@ -1,5 +0,0 @@
-src/main.o src/main.d: ../src/main.c ../src/LED/RGB.h ../src/main.h
-
-../src/LED/RGB.h:
-
-../src/main.h:
